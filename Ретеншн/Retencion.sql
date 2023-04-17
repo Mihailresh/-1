e20d0ae37c4d1d11863b0490656c36e38ce4fe4e
Михаил Решетников <mihail.resh.99@mail.ru>
2023-04-18 00:02:15 +0300

    Update Retencion.sql

--- Ретеншн/Retencion.sql
+++ Ретеншн/Retencion.sql
@@ -1,1 +1,11 @@
-
+SELECT date_trunc('month', start_date)::date start_month ,
+       start_date ,
+       date - start_date day_number ,
+       round(count(distinct user_id)::decimal / max(count(distinct user_id)) OVER (PARTITION BY start_date),
+             2) retention
+FROM   (SELECT user_id ,
+               time::date as date ,
+               min(time::date) OVER (PARTITION BY user_id) start_date
+        FROM   user_actions) t1
+GROUP BY start_date, date
+ORDER BY start_date, day_number
